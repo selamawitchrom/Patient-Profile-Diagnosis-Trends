6e331da67dba4c842ce5bd338f8b970f4e23fe80
Selamawit <selamawitaymero09@gmail.com>
2026-01-28 22:20:56 -0600

    Remove comments and clean up SQL queries

--- number_of _patient.sql
+++ number_of _patient.sql
@@ -1,11 +1,4 @@
 
-/* You are a Data Analyst for a mid-sized healthcare clinic. You’ve been given a synthetic dataset containing two tables:
-basic patient demographics (age, sex, race, insurance, ZIP code) and a diagnosis history containing ICD and CPT codes for each visit. 
-Your job is to help the clinic understand the makeup of its patient population and the most common conditions they treat.
-Doctors see patients one by one, but you get to see the big picture. Use this dataset to build simple insights about who your 
-patients are and what health issues are most common.*/
-
-
 select * from patient_visits;
 select * from  diagnosis;
 
@@ -26,7 +19,7 @@ group by cpt_code, patient_age, patient_sex
 
 ----
 
-/*What does the clinic’s patient population look like by Age?*/
+/*What does the clinicâ€™s patient population look like by Age?*/
 
  select patient_age, count(*) as patient 
 from  patient_visits
@@ -46,3 +39,4 @@ select top 5 cpt_code, count(*) as procedures
 from patient_visits
 group by cpt_code
 order by procedures desc 
+
